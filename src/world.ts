08200747321dc89e004ba36ee3e4de50014fbef6
towaueda <towa05300@gmail.com>
2023-01-31 00:16:02 +0900

    Fix world.ts

---
+++ src/world.ts
@@ -0,0 +1,16 @@
+export default class World {
+  message: string
+
+  constructor(message: string) {
+    this.message = message
+  }
+
+  public sayHello(elem: HTMLElement | null): void {
+    if (elem) {
+      elem.innerText = this.message
+    }
+
+
+    
+  }
+}
